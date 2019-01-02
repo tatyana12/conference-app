d41ac5f9fb63bd17e548abe6b8188a12225a86bf
tatyana12 <tatyana_pol@yahoo.com>
2019-01-01 20:24:50 -0800

    M

--- src/app/list-events.ts
+++ src/app/list-events.ts
@@ -1,12 +1,12 @@
 import { Event } from './event';
 
 export const EventsList: Event[] = [
-    { id: 10, name: 'Mikl', flag: 'S', summary: 'Vologda', room: '1975', presenter: '240,000' },
-    { id: 11, name: 'Diana', flag: 'S', summary: 'This seminar is about stock investing', room: 'S100', presenter: 'Warren Buffett' },
-    { id: 12, name: 'Dennis', flag: 'S', summary: 'This seminar is about investing in crypto currency', room: 'S100', presenter: 'Goldman Sachs'},
-    { id: 13, name: 'Natali', flag: 'S', summary: 'This seminar is about investing in commodity', room: 'S100', presenter: 'Fidelity'},
-    { id: 14, name: 'Oleg', flag: 'W', summary: 'This workshop is about learning JavaScript', room: 'W100', presenter: 'Prof. Silva'},
-    { id: 15, name: 'Antonina', flag: 'W', summary: 'This workshop is about Python', room: 'W100', presenter: 'Prof. Wostner'},
-    { id: 16, name: 'Maria', flag: 'W', summary: 'This workshop is about learning Ruby', room: 'W100', presenter: 'Prof. Strickland'},
-    { id: 17, name: 'Edvard', flag: 'W', summary: 'This workshop is about learning Java', room: 'W100', presenter: 'Prof. Schatz'},
+    { id: 1, name: 'Mikl', flag: 'S', summary: 'Vologda', room: '1975', presenter: '240,000' },
+    { id: 2, name: 'Diana', flag: 'S', summary: 'This seminar is about stock investing', room: 'S100', presenter: 'Warren Buffett' },
+    { id: 3, name: 'Dennis', flag: 'S', summary: 'This seminar is about investing in crypto currency', room: 'S100', presenter: 'Goldman Sachs'},
+    { id: 4, name: 'Natali', flag: 'S', summary: 'This seminar is about investing in commodity', room: 'S100', presenter: 'Fidelity'},
+    { id: 5, name: 'Oleg', flag: 'W', summary: 'This workshop is about learning JavaScript', room: 'W100', presenter: 'Prof. Silva'},
+    { id: 6, name: 'Antonina', flag: 'W', summary: 'This workshop is about Python', room: 'W100', presenter: 'Prof. Wostner'},
+    { id: 7, name: 'Maria', flag: 'W', summary: 'This workshop is about learning Ruby', room: 'W100', presenter: 'Prof. Strickland'},
+    { id: 8, name: 'Edvard', flag: 'W', summary: 'This workshop is about learning Java', room: 'W100', presenter: 'Prof. Schatz'},
 ];
