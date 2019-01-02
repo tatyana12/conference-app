b3f7ca96d8b76348bc6e608fcec8eb67401910d3
tatyana12 <tatyana_pol@yahoo.com>
2019-01-01 21:20:56 -0800

    M

--- src/app/list-events.ts
+++ src/app/list-events.ts
@@ -2,9 +2,9 @@ import { Event } from './event';
 
 export const EventsList: Event[] = [
     { id: 1, name: 'Mikl', flag: 'orange', summary: 'Vologda', room: '1975', presenter: '240,000' },
-    { id: 2, name: 'Diana', flag: 'S', summary: 'This seminar is about stock investing', room: 'S100', presenter: 'Warren Buffett' },
-    { id: 3, name: 'Dennis', flag: 'S', summary: 'This seminar is about investing in crypto currency', room: 'S100', presenter: 'Goldman Sachs'},
-    { id: 4, name: 'Natali', flag: 'S', summary: 'This seminar is about investing in commodity', room: 'S100', presenter: 'Fidelity'},
+    { id: 2, name: 'Diana', flag: 'orange', summary: 'This seminar is about stock investing', room: 'S100', presenter: 'Warren Buffett' },
+    { id: 3, name: 'Dennis', flag: 'orange', summary: 'This seminar is about investing in crypto currency', room: 'S100', presenter: 'Goldman Sachs'},
+    { id: 4, name: 'Natali', flag: 'orange', summary: 'This seminar is about investing in commodity', room: 'S100', presenter: 'Fidelity'},
     { id: 5, name: 'Oleg', flag: 'green', summary: 'This workshop is about learning JavaScript', room: 'W100', presenter: 'Prof. Silva'},
     { id: 6, name: 'Antonina', flag: 'green', summary: 'This workshop is about Python', room: 'W100', presenter: 'Prof. Wostner'},
     { id: 7, name: 'Maria', flag: 'green', summary: 'This workshop is about learning Ruby', room: 'W100', presenter: 'Prof. Strickland'},
